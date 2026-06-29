ee9f9e94744411741ea44268d08a5a452f4ee299
Anjali S Prabhu <anjali.s.prabhu18@gmail.com>
2026-06-29 16:50:26 +0530

    updated project files

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,10 +1,17 @@
 import Welcome from "./components/welcome";
 import NavBar from "./components/NavBar";
-function App() {
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+import Footer from "./components/Footer";
+function App(){
   return(
     <>
-      <NavBar />
-      <Welcome />
+    <NavBar />
+    <Welcome />
+    <br />
+    <CompanyCard />
+    <JobCard />
+    <Footer />
     </>
   )
 }
