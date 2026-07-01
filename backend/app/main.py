d023249564229b2a45fff203267dd5efde3c9347
Anjali S Prabhu <anjali.s.prabhu18@gmail.com>
2026-07-01 14:57:05 +0530

    Updated project files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from routers import company,job
+from routers import company,job,auth
 from database import Base,engine
-from models import company as company_model,job as job_model
+from models import company as company_model,job as job_model,users as user_model
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -16,7 +16,7 @@ app.add_middleware(
 )
 
 #Base.metadata.create_all(bind_engine)
-
+app.include_router(auth.router)
 app.include_router(company.router)
 app.include_router(job.router)
 
